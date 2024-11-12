293ed46f8e9621ae4f77dbcc3e661da166f56647
daveblac <onomondavid41@gmail.com>
2024-11-12 02:49:38 +0000

    picture acquisition

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,25 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import CompanyCard from "./CompanyCard"
+import Header from "./Header";
+import JobCard from "./JobCard";
+import JobTypes from "./JobTypes";
+import ListBox from "./ListBox";
+import Loading from "./Loading";
+import SignUp from "./SignUp";
+import TextInput from "./TextInput";
 
-
-export{Navbar, Footer, CustomButton};
+export {
+  CompanyCard,
+  CustomButton,
+  Footer,
+  Header,
+  JobCard,
+  JobTypes,
+  ListBox,
+  Loading,
+  Navbar,
+  SignUp,
+  TextInput,
+};
